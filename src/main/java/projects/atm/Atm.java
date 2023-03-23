381e6c90f39804a60ca3dbb62a819f2f4149e196
Will Marks <wjm8801@gmail.com>
2023-03-23 14:09:36 +1100

    changes to main

--- src/main/java/projects/atm/Atm.java
+++ src/main/java/projects/atm/Atm.java
@@ -1,11 +1,24 @@
 package projects.atm;
 
 
+import projects.atm.filehandling.FileHandling;
+
+import java.io.File;
+import java.util.List;
+
 public class Atm
 {
     public static void main( String[] args )
     {
 
+        FileHandling fileHandling = new FileHandling();
+        File userFile = FileHandling.getFile("/Users/williammarks/Documents/JavaClasses/atm_project/data/UserInfo.txt");
+        List<String[]> userInfoFile = fileHandling.getCSVContents(userFile, ",", false);
+
+
 
     }
-}
+
+
+    }
+
---
+++ src/main/java/projects/atm/filehandling/FileHandling.java
@@ -0,0 +1,48 @@
+package projects.atm.filehandling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileHandling {
+
+
+    public List<String[]> getCSVContents(File userFile, String separator, boolean readFirstLine) {
+
+        ArrayList<String[]> userInfoFile = new ArrayList<>();
+
+
+        if (userFile != null){
+            try (Scanner scanner = new Scanner(userFile)){
+                while (scanner.hasNextLine()){
+                    userInfoFile.add(scanner.nextLine().split(separator));
+                }
+            } catch (FileNotFoundException scannerException) {
+                System.out.println(scannerException);
+            }
+            if (readFirstLine) userInfoFile.remove(0);
+            return userInfoFile;
+        }
+        else return null;
+    }
+
+
+
+    public static File getFile(String filePath) {
+       try{
+           URL resource = getClass().getClassLoader().getResource(filePath);
+            if (resource == null){
+                throw new IllegalArgumentException("File not Found");
+            }else {
+                return new File(resource.toURI());
+            }
+        }
+        catch (URISyntaxException e) {
+            return null;
+        }
+    }
+}
