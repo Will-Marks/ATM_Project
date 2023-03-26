98dee5ad2ce112ae6da6771f253f9227f2eeaeae
Will Marks <wjm8801@gmail.com>
2023-03-27 10:44:42 +1100

    commit

--- src/test/java/projects/atm/filehandlingTest/FileHandlingTest.java
+++ src/test/java/projects/atm/filehandlingTest/FileHandlingTest.java
@@ -5,7 +5,11 @@ import org.junit.Test;
 import projects.atm.filehandling.FileHandling;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class FileHandlingTest {
 
@@ -14,12 +18,13 @@ public class FileHandlingTest {
 
         FileHandling filehandler = new FileHandling();
         File userFile = filehandler.getFile("data/UserInfo.txt");
-        List<String[]> users = filehandler.getCSVContents(userFile, ",", false);
-        for (String[] x: users) {
-            System.out.println(x);
+        List<String[]> users = filehandler.getCSVContents(userFile, ",", true);
+        for (String[] x : users) {
+            System.out.println(Arrays.toString(x));
 
+
+            assertEquals("John", users.get(0)[0]);
+            assertNotEquals("Leanne", users.get(0)[0]);
         }
-//        assertTrue(users.get(0)[0].equals("John"));
-//        assertFalse(users.get(0)[0].equals("Leanne"));
     }
 }
